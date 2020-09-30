f2eec86871642fb132ee6359634ca19f2609c9c9
Rafaela Polizel <rafaela.polizel@pca.com.br>
2020-09-30 16:50:01 -0300

    Add ESLint

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
-}
+const Repository: React.FC = () => <h1>Repository</h1>;
 
 export default Repository;
